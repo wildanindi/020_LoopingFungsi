69a9ab18e238c6cc02aac1a3f75939641c0436f5
wildanindi <wildanindi2@gmail.com>
2024-03-19 18:31:57 +0700

    add prosedur

--- Looping/Looping1.cpp
+++ Looping/Looping1.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+    int i;
+    string nama[5];
+
+    for (i = 150; i > 100; i-=10)
+    {
+    cout << i << ". " << "Selamat Berbuka" << endl;
+    }
+
+    cout << "Nilai i terakhir = " << i << endl;
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukkan Nama ke-" << i << endl;
+        cin >> nama[i];
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Nama ke-" << i << ": " << nama[i] << endl;
+    }
+}
