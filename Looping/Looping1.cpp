c2d76b980b2d9f638a14b733336842baba4d33e6
wildanindi <wildanindi2@gmail.com>
2024-03-19 18:58:01 +0700

    add command di for untuk memberi tahu prosedur perulangan

--- Looping/Looping1.cpp
+++ Looping/Looping1.cpp
@@ -8,19 +8,19 @@ int main(){
     int i;  // variabel
     string nama[5]; // variabel
 
-    for (i = 150; i > 100; i-=10)
+    for (i = 150; i > 100; i-=10) //prosedur perulangan yang menggunakan for
     {
     cout << i << ". " << "Selamat Berbuka" << endl;
     }
 
     cout << "Nilai i terakhir = " << i << endl;
 
-    for (i = 0; i < 5; i++){
+    for (i = 0; i < 5; i++){    // prosedur perulangan yang menggunakan for
         cout << "Masukkan Nama ke-" << i << endl;
         cin >> nama[i];
     }
 
-    for (i = 0; i < 5; i++){
+    for (i = 0; i < 5; i++){    // prosedur perulangan yang menggunakan for
         cout << "Nama ke-" << i << ": " << nama[i] << endl;
     }
 }
