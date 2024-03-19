5c355f7a96353373d8a7e8573a8df7f48484d044
wildanindi <wildanindi2@gmail.com>
2024-03-19 19:13:29 +0700

    menambahkan proses input yang nantinya akan di simpan ke dalam variabel yang telah kita tentukan

--- Looping/ProsedurFungsi.cpp
+++ Looping/ProsedurFungsi.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int nTelor, nMie, nAir;
+int htelor = 2000, hMie = 2800, hAir = 3000;
+string nama;
+
+void input ()
+{
+    cout << "Masukkan Nama = ";
+    cin >> nama;
+    cout << "Masukkan Jumlah Telor = ";
+    cin >> nTelor;
+    cout << "Masukkan Jumlah Mie = ";
+    cin >> nMie;
+    cout << "Masukkan Jumlah Air Mineral = ";
+    cin >> nAir;
+}
